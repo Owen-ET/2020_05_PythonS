695568131376468786fbceff8eaaac18056ec775
zhangchang <zhangchanget@126.com>
2020-09-03 19:08:48 +0800

    20200903

---
+++ String.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/09/03 18:34
+# @Author  : zc
+# @File    : String.py
+
+
+
+data_string = "hello world"
+
+# 1、截断：
+print(data_string[:])
+print(data_string[1:3])
+print(data_string[:-3])
+print(data_string[0:])
+print(data_string[:5:-1])
+
+# 2、字符串的链接
+print(data_string + "21 day")
